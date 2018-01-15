73fc66148c9109ce3add8d1c0e7b02132cc5e885
mlourenc <mario.lourenco.ext@coriant.com>
2018-01-15 15:10:10 +0000

    project euler #1 - clean solution

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
-use std::collections::LinkedList;
 
 fn main() {
-    println!("Hello, world!");
-    let number = 1000_i32;
-    let mut list : LinkedList<i32> = LinkedList::new();
-    for cur in 1..number {
-        if(cur % 3) == 0 || (cur % 5) == 0{
-            list.push_back(cur);
-        }
-    }
+    let sequence = 0..10000_i32;
+    let values = sequence
+        .filter(|v| v % 3 == 0 || v % 5 == 0)
+        .collect::<Vec<i32>>();
 
-    let mut it = list.iter();
-    loop{
-        let mut elem :Option<&i32> = it.next();
-        let x = *elem.get_or_insert(&-1);
-        if *x ==-1 {
-            break;
-        }
-        println!("x = {}", x);
+    for x in values.iter(){
+        println!("X = {}", x);
     }
 }
