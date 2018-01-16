4470eddfe319d00f6f9251c501653ee738fd54d2
mariomilha <mariomilha@gmail.com>
2018-01-16 21:08:41 +0000

    Added the question on the comments

--- src/euler3.rs
+++ src/euler3.rs
@@ -1,4 +1,8 @@
+/**
 
+    The prime factors of 13195 are 5, 7, 13 and 29.
+    What is the largest prime factor of the number 600851475143 ?
+*/
 pub fn execute() {
     let mut number: u64 = 600851475143;
     let mut divisor: u64 = 2;
