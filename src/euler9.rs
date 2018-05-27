9c06360574ec0ad13d5133fee3bc6a1d9677714f
mariomilha <mariomilha@gmail.com>
2018-05-27 16:46:48 +0100

    euler 9 complete

--- src/euler9.rs
+++ src/euler9.rs
@@ -11,5 +11,59 @@ Find the product abc.
 */
 
 pub fn execute(){
-    
+    let pythagorean: Pythagorean = Pythagorean::new();
+    let result = pythagorean.filter(|p| p.0 + p.1 + p.2 == 1000)
+        .filter(|p| is_square_sum(p.0, p.1, p.2))
+        .take(1)
+        .next()
+        .unwrap();
+
+    println!("result found - {:?}", result);
+    println!("result is {}", result.0 * result.1 * result.2);
+}
+
+fn is_square_sum(a:u32, b:u32, c:u32) -> bool{
+    let sq_a = a * a;
+    let sq_b = b * b;
+    let sq_c = c * c;
+    return (sq_a + sq_b) == sq_c; 
+}
+
+struct Pythagorean{
+    a:u32,
+    b:u32,
+    c:u32,
+}
+
+impl Pythagorean{
+    fn new()-> Pythagorean{
+        Pythagorean{a: 1, b: 2, c: 3}
+    }
+}
+
+impl Iterator for Pythagorean{
+    type Item = (u32,u32,u32);
+
+    fn next(&mut self) -> Option<Self::Item>{
+        self.c = self.c + 1;
+        if !sum_greater_than_thousand(self.a, self.b, self.c) {
+            return Some((self.a, self.b, self.c));
+        }
+        self.b = self.b + 1;
+        self.c = self.b + 1;
+        if !sum_greater_than_thousand(self.a, self.b, self.c){
+            return Some((self.a, self.b, self.c));
+        }
+        self.a = self.a + 1;
+        self.b = self.a + 1;
+        if !sum_greater_than_thousand(self.a, self.b, self.c){
+            return Some((self.a, self.b, self.c));
+        }
+        return None;
+    }
+}
+
+fn sum_greater_than_thousand(a:u32, b:u32, c:u32) -> bool{
+    let sum = a + b + c;
+    return sum > 1000;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 extern crate time;
 
-mod euler8;
+mod euler9;
 
 fn main() {
    let start_time = time::get_time().nsec;
    println!("start running");
-   euler8::execute();
+   euler9::execute();
    println!("finished in - {} nano seconds", time::get_time().nsec - start_time);
 }
