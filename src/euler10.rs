bb25c23ef959cb973a690560bff7dbcb16d6f117
mariomilha <mariomilha@gmail.com>
2018-06-12 12:45:14 +0100

    euler10 done

--- src/euler10.rs
+++ src/euler10.rs
@@ -4,6 +4,39 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 */
 
-fn execute(){
-    
+pub fn execute() {
+    let mut number : u64 = 3;
+    let mut primes : Vec<u64> = Vec::new();
+    primes.push(2);
+
+    while number < 2_000_000 {
+        let prime = check_already_found_primes(number, &primes);
+        if prime != 0 {
+            println!("Found new prime: {}", prime);
+            primes.push(prime);
+            println!("primes len = {}", primes.len());
+        }
+        number = number + 1;
+    }
+    let result:u64 = primes.iter().sum();
+    println!("The sum of all primes is = {}", result);
+}
+
+fn check_already_found_primes(to_test:u64, primes: &Vec<u64>) -> u64{
+    if to_test % 2 == 0 {
+        return 0;
+    }
+    for prime in primes {
+        if to_test % prime == 0{
+            return 0;
+        }
+    }
+    let mut last_number = primes[primes.len() - 1];
+    while last_number < to_test {
+        if to_test % last_number == 0{
+            return 0;
+        }
+        last_number = last_number + 1;
+    }
+    return last_number;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 extern crate time;
 
-mod euler9;
+mod euler10;
 
 fn main() {
    let start_time = time::get_time().nsec;
    println!("start running");
-   euler9::execute();
+   euler10::execute();
    println!("finished in - {} nano seconds", time::get_time().nsec - start_time);
 }
